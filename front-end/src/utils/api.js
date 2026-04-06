d4dfd5db18e5ff1973f0e274509f8f153cbad0c6
Samyo Pramanik <samyopramanik2003@gmail.com>
2026-04-06 23:40:45 +0600

    frontend setup

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
-  withCredentials: true, 
+  baseURL: '/api',
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   }
