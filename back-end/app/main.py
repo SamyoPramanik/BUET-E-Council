d76786b932d60bee5b6e40a3ccdf53f3062205a4
Samyo Pramanik <samyopramanik2003@gmail.com>
2026-04-06 23:40:31 +0600

    backend setup

--- back-end/app/main.py
+++ back-end/app/main.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .config import settings
-from .api import auth, users, meetings, participants, agendas, resolutions, files, signature_cards
+from .api import auth, users, meetings, participants, agendas, resolutions, files, signature_cards, health
 
 from .database import init_db, engine
 from .admin import setup_admin
@@ -44,7 +44,7 @@ app.include_router(agendas.router)
 app.include_router(resolutions.router)
 app.include_router(files.router)
 app.include_router(signature_cards.router)
-
+app.include_router(health.router)
 # Mount the Admin Interface
 setup_admin(app, engine)
 
